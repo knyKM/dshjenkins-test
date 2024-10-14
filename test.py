e51d29c950d85c8abc5f9620bf3d3723591b5ed9
knyKM <kmelo249@gmail.com>
2024-10-14 23:55:19 +0000

    alter for test

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print('Hello World')
+import pytest
+import time
+from selenium import webdriver
+
+@pytest.fixture(scope="module")
+
+def browser():
+    chromeOptions = webdriver.ChromeOptions()
+    chromeOptions.add_argument('--headless')
+    chromeOptions.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(options=chromeOptions)
+    yield driver
+    driver.quit()
